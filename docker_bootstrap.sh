dcb2b3437fca4db5a694c3f9db7ea542ff40101a
lazarev <diedsmiling@gmail.com>
2016-06-12 13:23:31 +0300

    docker configs fixed

--- docker_bootstrap.sh
+++ docker_bootstrap.sh
@@ -4,5 +4,5 @@ docker stop iamaplayer
 docker rm iamaplayer
 docker rmi diedsmiling/iamaplayer
 docker build -t diedsmiling/iamaplayer .
-docker run -d -p 49170:3000 --name iamaplayer -v $HOME/projects/iamaplayer_backend/:/usr/src/app diedsmiling/iamaplayer tail -f /dev/null
-docker exec -it iamaplayer /bin/bash
+docker run -d -p 49170:3000 --name iamaplayer -v $HOME/projects/iamaplayer_graphql_server/:/usr/src/app diedsmiling/iamaplayer tail -f /dev/null
+docker logs --follow iamaplayer
--- docker_entry.sh
+++ docker_entry.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "******INSTALL NODE MODULES******"
 npm install
-nodemon
+nodemon -L
